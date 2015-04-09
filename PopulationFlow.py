f86214c4b86428d59a1bd508d7978f385d315f86
Ryan Brideau <ryan.brideau@gmail.com>
2015-04-08 23:57:37 -0300

    Added geolocation file

--- PopulationFlow.py
+++ PopulationFlow.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[58]:
+# In[1]:
 
 import pandas as pd
 from pandas import DataFrame, Series
@@ -19,20 +19,20 @@ sys.setdefaultencoding("utf-8")
 from pandas.compat import u
 
 
-# In[22]:
+# In[2]:
 
 get_ipython().magic(u'pdb off')
 
 
 # **Source: **[In-, out- and net-migration estimates, by geographic regions of origin and destination, *Terminated*](http://www5.statcan.gc.ca/cansim/a26?lang=eng&retrLang=eng&id=1110030&paSer=&pattern=&stByVal=1&p1=1&p2=-1&tabMode=dataTable&csid=)
 
-# In[62]:
+# In[3]:
 
 flowData = pd.read_csv('../TableD_01110030-eng.csv')
 flowData.head()
 
 
-# In[63]:
+# In[4]:
 
 # Convert place names to unicode
 flowData['GEO'] = flowData['GEO'].map(u)
@@ -40,7 +40,7 @@ flowData['GEODEST'] = flowData['GEODEST'].map(u)
 flowData.head()
 
 
-# In[64]:
+# In[5]:
 
 # Remove unneeded columns
 dropCols = ['Geographical classification',
@@ -51,26 +51,26 @@ flowData = flowData.drop(dropCols, axis=1)
 flowData.head(10)
 
 
-# In[75]:
+# In[6]:
 
 # Rename columns
 flowData = flowData.rename(columns={"GEO": "Origin", "GEODEST": "Destination"})
 
 
-# In[76]:
+# In[7]:
 
 # Filter for only the most recent data
 flowData2011 = flowData[flowData['Ref_Date'] == 2011].drop('Ref_Date', axis=1).reset_index(drop=True)
 flowData2011.head()
 
 
-# In[77]:
+# In[8]:
 
 # Convert that Value column to a numeric data type
 flowData2011['Value'] = flowData2011['Value'].convert_objects(convert_numeric=True)
 
 
-# In[78]:
+# In[9]:
 
 # Remove all the non-census areas so we can geocode the cities that qualify as CMAs
 flowData2011_cma = flowData2011[~flowData2011['Destination'].str.contains('Non-census')]
@@ -78,21 +78,33 @@ flowData2011_cma = flowData2011_cma[~flowData2011_cma['Origin'].str.contains('No
 flowData2011_cma.head()
 
 
-# In[90]:
+# In[10]:
 
 outMig = flowData2011_cma[flowData2011_cma['MIGMOVE'] == "Out-migration"].drop('MIGMOVE', axis=1).reset_index(drop=True)
 outMig.head()
 
 
-# In[91]:
+# In[11]:
 
 outMigPiv = outMig.pivot('Origin', 'Destination', 'Value')
 outMigPiv.head()
 
 
-# In[93]:
+# In[12]:
 
-sns.heatmap(outMigPiv)
+# Since there is such a range in values, let's put this on a log scale
+log_scale = lambda x: np.log10(x)
+outMigPivLog = outMigPiv.applymap(log_scale).replace([np.inf, -np.inf], 0)
+
+
+# In[13]:
+
+sns.heatmap(outMigPivLog)
+
+
+# In[ ]:
+
+sns.clustermap(outMigPivLog)
 
 
 # In[ ]:
